7078e0298ae5d9c20485e61d045bb3ed4842db07
нα¢кєяχ <67502949+HackeringX@users.noreply.github.com>
2023-11-16 13:40:16 -0600

    Update Computer.java
    Comment

--- Family Feud5/src/main/java/com.familyfeud.code/Computer.java
+++ Family Feud5/src/main/java/com.familyfeud.code/Computer.java
@@ -37,6 +37,10 @@ public class Computer extends Player{
         return difficulty;
     }
 
+    /**
+    * While also setting the difficulty, it also sets the accuracy of the robot depending on the difficulty
+    * @param The difficulty
+    */
     public void setDifficulty(String difficulty) {
 
         this.difficulty = difficulty;
@@ -60,6 +64,9 @@ public class Computer extends Player{
         name = "Computer";
     }
 
+    /**
+    * This is to set the times when the computer will answer a question
+    */
     public void startRound() {
         for (int i = 0; i < 6; i++) {
             int randTime = ((int) (Math.random() * 10)) + (i * 10);
@@ -67,6 +74,13 @@ public class Computer extends Player{
         }
     }
 
+    /**
+    * It calculates what the computer will do:
+    * 1. Answer the question - It will either get it right (gains however many points the question is worth) or wrong (returns -5). It might get many right initially, but loses momentum
+    * 2. Not answer the question
+    * @param The current time left
+    * @return The result (positive - gets that many points, -5 - incorrect, 0 - nothing)
+    */
     public int playGame(int time) {
 
         if (Arrays.stream(worths).sum() == 0) {
