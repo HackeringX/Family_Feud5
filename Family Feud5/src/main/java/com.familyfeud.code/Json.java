3081f26eaa61e372b37af11e8bbd04e09f69a1de
нα¢кєяχ <67502949+HackeringX@users.noreply.github.com>
2023-11-16 14:05:36 -0600

    Update Json.java
    Comment

--- Family Feud5/src/main/java/com.familyfeud.code/Json.java
+++ Family Feud5/src/main/java/com.familyfeud.code/Json.java
@@ -9,12 +9,21 @@ public class Json {
     
     private static ObjectMapper objectMapper = getDefaultObjectMapper();
 
+    /**
+    * Configures object mapper
+    * @return The object mapper
+    */
     private static ObjectMapper getDefaultObjectMapper() {
         ObjectMapper defaultObjectMapper = new ObjectMapper();
         defaultObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return defaultObjectMapper;
     }
 
+    /**
+    * Returns the JsonNode (From Jackson library)
+    * @param JSONL string
+    * @return JsonNode
+    */
     public static JsonNode parse(String src) throws JsonProcessingException {
         return objectMapper.readTree(src);
     }
